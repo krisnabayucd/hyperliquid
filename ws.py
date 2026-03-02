f74fd409176aa50ad2429543ad1d360e18e93e24
unknown <krishnabayu18@gmail.com>
2026-03-02 12:43:23 +0700

    update ws.py

--- ws.py
+++ ws.py
@@ -1,4 +1,5 @@
-from hyperliquid.exchange import Exchange, Info
+from hyperliquid.exchange import Exchange
+from hyperliquid.info import Info
 import utils
 import time
 
@@ -6,7 +7,19 @@ _, _, base_url = utils.get_config()
 info = Info(base_url=base_url, skip_ws=False)
 
 def callback(msg):
-    print("Update message:", msg)
+    if msg.get("channel") == "l2Book":
+        data = msg["data"]
+        bids = data["levels"][0][:3]
+        asks = data["levels"][1][:3]
+        print(f"BTC | Best bid: {bids[0]['px']} | Best ask: {asks[0]['px']}")
+    else:
+        print("Other message:", msg)
 
-info.subscribe({"type": "l2_book", "coin": "BTC"}, callback)
-time.sleep(10)  # Keep the subscription alive for 10 seconds``
+info.subscribe({"type": "l2Book", "coin": "BTC"}, callback)
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Shutting down...")
+    info.disconnect()
